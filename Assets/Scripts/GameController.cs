c9aba2e3511c9053310c8c3f789e3d4267f50a21
tangtrunghieu99 <58714859+tangtrunghieu99@users.noreply.github.com>
2020-11-21 13:08:07 +0700

    UpLanHai

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject obstacle;
+    public float spawnTime;
+    float m_spawnTime;
+    int isScore;
+    bool isGameover;
+    UIManager m_UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_spawnTime = 0;
+        m_UI = FindObjectOfType<UIManager>();
+        m_UI.SetScoreText("Score: " + isScore);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGameover)
+        {
+            m_spawnTime = 0;
+            m_UI.ShowGameoverPanel(true);
+            return;
+        }
+        m_spawnTime -= Time.deltaTime;
+        if(m_spawnTime<=0)
+        {
+            SpawnOstacle();
+            m_spawnTime = spawnTime;
+        }
+    }
+    public void SpawnOstacle()
+    {
+        float randYpos = Random.Range(-2.75f, -1f);
+        Vector2 spawnPos = new Vector2(11, randYpos);
+        if(obstacle)
+        {
+            Instantiate(obstacle, spawnPos, Quaternion.identity);
+        }
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void SetScore( int value )
+    {
+        isScore = value;
+    }
+    public int GetScore()
+    {
+        return isScore;
+    }
+    public void ScroreIncrement()
+    {
+        isScore++;
+        m_UI.SetScoreText("Score: " + isScore);
+    }
+    public bool IsGameover()
+    {
+        return isGameover;
+    }
+    public void SetGameoverState(bool state)
+    {
+        isGameover = state;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce;
+    Rigidbody2D rb;
+    bool isGround;
+    GameController m_gc;
+    public AudioSource auS;
+    public AudioClip jumpSound;
+    public AudioClip loseSound; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        m_gc = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isJumpKeyPressed = Input.GetKeyDown(KeyCode.Space);
+        if(isJumpKeyPressed && isGround)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            isGround = false;
+            if (auS && jumpSound)
+            {
+                auS.PlayOneShot(jumpSound);
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {   
+        if(col.CompareTag("Obstacle"))
+        {
+            if(auS && loseSound )
+            {
+                auS.PlayOneShot(loseSound);
+            }
+            m_gc.SetGameoverState(true);
+        }
+    }
+}
